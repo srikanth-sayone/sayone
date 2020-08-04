c4d5f26f4b2465bda6ca7540218c2ce0b4a1c067
Srikanth Naredla <sn19162@corp.ojas-it.com>
2020-08-04 10:25:15 +0530

    sayone

---
+++ SpringConfigServer/insurance-config-server/src/test/java/com/javatechie/spring/insurance/server/api/InsuranceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.spring.insurance.server.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
